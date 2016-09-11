b7ab6ef737848144f498f29c3cb8b556586f3498
ZeroUnderscoreOu <ZeroUnderscoreOu@Yandex.ru>
2016-09-11 16:24:48 +0300

    Does he see BBCodes?

---
+++ Beholder.js
@@ -0,0 +1,20 @@
+console.log("SBBC");
+// little easteregg
+if (GM_getValue("Beholder")==undefined&&document.getElementById("account_pulldown").textContent.toLocaleUpperCase().includes("BEHOLDER")) {
+	GM_setValue("Beholder","ICU");
+};
+
+function Beholder(HeSeesUs) {
+	if (typeof(HeSeesUs.detail)=="boolean") {
+		GM_setValue("Beholder",HeSeesUs);
+	};
+	return !!GM_getValue("Beholder"); // returning eye state
+};
+
+function SteamBBCodesBeholder(State) {
+	document.dispatchEvent(new CustomEvent("Behold",{detail:State}));
+};
+
+document.addEventListener("Behold",Beholder,false);
+window.eval("var SteamBBCodesBeholder = "+SteamBBCodesBeholder+";");
+console.log("SHBO");

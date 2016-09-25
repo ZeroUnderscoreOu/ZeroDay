8bbbebf473019be95a84b17cf372504256f08c69
ZeroUnderscoreOu <ZeroUnderscoreOu@Yandex.ru>
2016-09-25 08:21:08 +0300

    Steam BBCodes - can I tamper a monkey?

--- Beholder.js
+++ Beholder.js
@@ -1,8 +1,6 @@
-console.log("Begin");
 // little easteregg
 var Pulldown = document.getElementById("account_pulldown"); // account menu with nickname
 var SteamBBCodes;
-console.log(1,GM_getValue("Beholder"));
 if (Pulldown
 	&&Pulldown.textContent.toLocaleUpperCase().includes("BEHOLDER")
 	&&GM_getValue("Beholder")==undefined) {
@@ -12,13 +10,12 @@ if (GM_getValue("Beholder")) {
 	console.log("You are Beholder! Type \"SteamBBCodes.Blink()\" here and reload the page to close your eye and pretend to be normal.");
 };
 SteamBBCodes = // Chrome doesn't support Object.toSource()
-	'{' /
-		'HeSeesUs: ' + GM_getValue("Beholder") + ',' /
-		'Blink: function(){' /
-			'document.dispatchEvent(new Event("Blink"));' /
-		'}' /
+	'{' +
+		'HeSeesUs: ' + GM_getValue("Beholder") + ',' +
+		'Blink: function(){' +
+			'document.dispatchEvent(new Event("Blink"));' +
+		'}' +
 	'};';
-console.log(2,SteamBBCodes);
 
 function BlinkBack() {
 	GM_setValue("Beholder",!GM_getValue("Beholder"));
@@ -26,12 +23,3 @@ function BlinkBack() {
 
 document.addEventListener("Blink",BlinkBack,false);
 window.eval("var SteamBBCodes = "+SteamBBCodes);
-var Userscript = document.createElement("Script");
-Userscript.type = "Text/JavaScript";
-Userscript.textContent =
-	"(function(){" /
-	GM_getResourceText("SteamBBCodes") /
-	"})();";
-console.log(3,Userscript);
-document.head.appendChild(Userscript);
-console.log("End");
